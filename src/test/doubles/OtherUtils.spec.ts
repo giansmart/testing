e2b2e428d40bd1241e8d03619d15593d0681a83d
Giancarlo Poemape <gipo@Giancarlos-MacBook-Pro.local>
2024-02-28 23:45:47 -0500

    adding spy tests

--- src/test/doubles/OtherUtils.spec.ts
+++ src/test/doubles/OtherUtils.spec.ts
@@ -1,8 +1,35 @@
-import { calculateComplexity, toUpperCaseWithCallback } from "../../app/doubles/OtherUtils"
+import { OtherStringUtils, calculateComplexity, toUpperCaseWithCallback } from "../../app/doubles/OtherUtils"
 
 describe('OtherUtils test suite', () => {
 
-    describe.only('Tracking callbacks with Jest mocks', () => {
+    describe.only('OtherStringUtils test with spies', () => {
+        let sut: OtherStringUtils;
+
+        beforeEach(() => {
+            sut = new OtherStringUtils();
+        });
+
+        test('Use a spy to track calls', () => {
+            const toUpperCaseSpy = jest.spyOn(sut, 'toUpperCase');
+            sut.toUpperCase('abc');
+            expect(toUpperCaseSpy).toHaveBeenCalledWith('abc');
+        });
+
+        test('Use a spy to track calls to another module', () => {
+            const consoleLogSpy = jest.spyOn(console, 'log');
+            sut.logString('abc');
+            expect(consoleLogSpy).toHaveBeenCalledWith('abc');
+        });
+
+        test.only('Use a spy to replace implementation of a method', () => {
+            jest.spyOn(sut as any, 'callExternalService').mockImplementation(() => {
+                console.log('calling mocked implementation!!')
+            });
+            (sut as any).callExternalService();
+        })
+    });
+
+    describe('Tracking callbacks with Jest mocks', () => {
 
         const callbackMock = jest.fn();
 
@@ -25,7 +52,7 @@ describe('OtherUtils test suite', () => {
         });
     });
 
-    describe.only('Tracking callbacks', () => {
+    describe('Tracking callbacks', () => {
         let callbackArgs = [];
         let timesCalled = 0;
 
