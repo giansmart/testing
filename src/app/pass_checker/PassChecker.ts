3befbd2b0b880ab1c99b81cc633fbee93afbbcf2
Giancarlo Poemape <gipo@Giancarlos-MacBook-Pro.local>
2024-02-28 19:04:32 -0500

    new requirement: admin password and refactoring

--- src/app/pass_checker/PassChecker.ts
+++ src/app/pass_checker/PassChecker.ts
@@ -1,7 +1,8 @@
 export enum PasswordErrors {
     SHORT = 'Password is too short!',
     NO_UPPER_CASE = 'Upper case letter required!',
-    NO_LOWER_CASE = 'Lower case letter required!'
+    NO_LOWER_CASE = 'Lower case letter required!',
+    NO_NUMBER = 'At least one number is required'
 }
 
 export interface CheckResult {
@@ -13,20 +14,48 @@ export class PasswordChecker {
     public checkPassword(password: string): CheckResult {
         const reasons: PasswordErrors[] = [];
 
-        if(password.length < 8) {
-            reasons.push(PasswordErrors.SHORT)
-        }
+        this.checkForLength(password, reasons);
+        this.checkForUpperCase(password, reasons);
+        this.checkForLowerCase(password, reasons);
 
-        if(password === password.toLowerCase()) {
-            reasons.push(PasswordErrors.NO_UPPER_CASE)
-        }
-
-        if(password === password.toUpperCase()) {
-            reasons.push(PasswordErrors.NO_LOWER_CASE)
-        }
         return {
             valid: reasons.length === 0 ? true : false,
             reasons: reasons
         };
+    }
+
+    public checkAdminPassword(password: string): CheckResult {
+        const basicCheck = this.checkPassword(password);
+        this.checkForNumber(password, basicCheck.reasons);
+
+        return {
+            valid: basicCheck.reasons.length === 0 ? true : false,
+            reasons: basicCheck.reasons
+        };
+    }
+
+    private checkForNumber(password: string, reasons: PasswordErrors[]) {
+        const hasNumber = /\d/;
+        if(!hasNumber.test(password)) {
+            reasons.push(PasswordErrors.NO_NUMBER);
+        }
+    }
+
+    private checkForLength(password: string, reasons: PasswordErrors[]) {
+        if(password.length < 8) {
+            reasons.push(PasswordErrors.SHORT);
+        }
+    }
+
+    private checkForUpperCase(password: string, reasons: PasswordErrors[]) {
+        if(password === password.toLowerCase()) {
+            reasons.push(PasswordErrors.NO_UPPER_CASE);
+        }
+    }
+
+    private checkForLowerCase(password: string, reasons: PasswordErrors[]) {
+        if(password === password.toUpperCase()) {
+            reasons.push(PasswordErrors.NO_LOWER_CASE)
+        }
     }
 }
--- src/test/pass_checker/PassChecker.spec.ts
+++ src/test/pass_checker/PassChecker.spec.ts
@@ -48,4 +48,16 @@ describe('PasswordChecker test suite', () => {
         expect(actual.valid).toBe(true);
         expect(actual.reasons).toHaveLength(0)
     });
+
+    it('should check if Admin password with no number is invalid', () => {
+        const actual = sut.checkAdminPassword('ABCDabcd');
+        expect(actual.valid).toBe(false);
+        expect(actual.reasons).toContain(PasswordErrors.NO_NUMBER)
+    });
+
+    it('should check if Admin password with number is valid', () => {
+        const actual = sut.checkAdminPassword('ABCDabcd12');
+        expect(actual.valid).toBe(true);
+        expect(actual.reasons).not.toContain(PasswordErrors.NO_NUMBER)
+    });
 })
