e6e7dedb6c3b52c2ca2a2de4a5883e4378e322b1
Giancarlo Poemape <gipo@Giancarlos-MacBook-Pro.local>
2024-02-28 20:10:32 -0500

    adding mock with manual tracking callback

--- src/test/doubles/OtherUtils.spec.ts
+++ src/test/doubles/OtherUtils.spec.ts
@@ -2,6 +2,36 @@ import { calculateComplexity, toUpperCaseWithCallback } from "../../app/doubles/
 
 describe('OtherUtils test suite', () => {
 
+    describe.only('Tracking callbacks', () => {
+        let callbackArgs = [];
+        let timesCalled = 0;
+
+        function callbackMock(arg: string) {
+            callbackArgs.push(arg);
+            timesCalled++;
+        }
+
+        afterEach(() => {
+            callbackArgs = [];
+            timesCalled = 0;
+        })
+
+        it('should test toUpperCaseWithCallback with invalid argument - track calls', () => {
+            const actual = toUpperCaseWithCallback('', callbackMock);
+            expect(actual).toBeUndefined();
+            expect(callbackArgs).toContain('Invalid argument!')
+            expect(timesCalled).toBe(1);
+        });
+
+        it('should test toUpperCaseWithCallback with valid argument - track calls', () => {
+            const actual = toUpperCaseWithCallback('abc', callbackMock);
+            expect(actual).toBe('ABC');
+            expect(callbackArgs).toContain('called function with abc')
+            expect(timesCalled).toBe(1);
+        });
+
+    })
+
     it('should test toUpperCaseWithCallback with invalid argument', () => {
         const actual = toUpperCaseWithCallback('', ()=> {});
         expect(actual).toBeUndefined();
